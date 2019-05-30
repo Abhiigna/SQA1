671ba619de7cad329c25d6defe157c5b1cbabeaf
Abhigna Gadda <agadda1432@conestogac.on.ca>
2019-05-30 00:52:55 -0400

    Rectangle Class Created

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,8 +8,77 @@ namespace Assignment1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int validateMenuSelection()
         {
+            string userInput = "";
+            bool validMenuSelect = false;
+
+            while (validMenuSelect == false)
+            {
+                Console.WriteLine("1 = Get Rectangle Length");
+                Console.WriteLine("2 = Change Rectangle Length");
+                Console.WriteLine("3 = Get Rectangle Width");
+                Console.WriteLine("4 = Change Rectangle Width");
+                Console.WriteLine("5 = Get Rectangle Perimeter");
+                Console.WriteLine("6 = Get Rectangle Area");
+                Console.WriteLine("7 = Exit\n");
+
+                Console.WriteLine("Please enter your option:\n");
+                userInput = Console.ReadLine();
+
+                if (userInput != "1" &&
+                   userInput != "2" &&
+                   userInput != "3" &&
+                   userInput != "4" &&
+                   userInput != "5" &&
+                   userInput != "6" &&
+                   userInput != "7")
+                {
+                    Console.WriteLine("Option invalid:\n");
+
+                }
+                else
+                {
+                    validMenuSelect = true;
+                }
+            }
+            Console.WriteLine();
+            return int.Parse(userInput);
         }
-    }
+
+        public static int validUserInput(string rectSide)
+        {
+            int number = 1;
+            bool isValid = false;
+
+            while (isValid == false)
+            {
+                Console.WriteLine($"Please enter the {rectSide} of your rectangle:");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+
+                bool result = int.TryParse(userInput, out number);
+
+                if (result == false)
+                {
+                    Console.WriteLine("Invalid Input, please try again.\n");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Number is less than 0, please give a number greater than 0.\n");
+                }
+                else
+                {
+                    isValid = true;
+
+                }
+
+            }
+        }
+
+
+                
+
+
+}
 }
